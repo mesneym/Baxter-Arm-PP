f3aa4ceab88c387fe19ded23bcb77027de0a6615
Eashwar Sathyamurthy <eashwar.sathyamurthy@gmail.com>
2020-04-15 15:00:33 -0400

    Working version

--- ros_ws/src/baxter_simulator/baxter_sim_examples/scripts/ik_pick_and_place_demo.py
+++ ros_ws/src/baxter_simulator/baxter_sim_examples/scripts/ik_pick_and_place_demo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2d2444c9761977c9c3a196ea51faa095f4bb7c0c439ee06f29ff56af08fd3c1
-size 15384
+oid sha256:16ad8578b7cbd9b34d6f00a3521d059eb83421e3a8929bb8e554aba8ae51a21a
+size 16490
